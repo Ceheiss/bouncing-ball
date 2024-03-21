f347de7ea8a2289a984da86ea3e762298eec278f
Cristóbal Heiss <barbacoabq@gmail.com>
2024-03-21 14:25:11 +0100

    Update constants.ts

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,6 @@
 import { KameleoonUtils } from "@kameleoon/javascript-sdk";
 
 // -- Constants values
-export const siteCode = "2p2rgz4sbp";
+export const siteCode = "guctkp99g8";
 export const featureKey = "bouncing_ball_demo";
 export const visitorCode = KameleoonUtils.getVisitorCode("www.example.com");
